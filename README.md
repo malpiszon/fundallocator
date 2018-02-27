e7959fd076287b18f84aac9adfc5e3c6d03dd84f
alan <alan@malpiszon.net>
2018-02-27 18:41:32 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Blue Services Task" 
---
+++ src/main/java/net/malpiszon/bstask/BstaskApplication.java
@@ -0,0 +1,12 @@
+package net.malpiszon.bstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BstaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BstaskApplication.class, args);
+	}
+}
